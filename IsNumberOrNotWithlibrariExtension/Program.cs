897e9e8803baee17c5b3725cf58e90eea5e14dc7
Retoto <brecas1820@gmail.com>
2026-02-12 18:37:16 -0500

    Create Blank solution

---
+++ IsNumberOrNotWithlibrariExtension/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Share;// con este utilizamos la libreria que creamos en Share
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };// definimos una lista de opciones para validar la respuesta del usuario
+do
+{
+    var number = ConsoleExtension.GetInt("Ingrese Numero entero o ctr c para salir :"); //ya solo llamamos la funcion que creamos en la libreria se ahorra la verificacion del punto pasado
+
+
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine($"el numero {number} es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {number} es Impar.");
+        }
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Desea continuar [S]i , [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));// validamos que la respuesta del usuario sea igual a alguna de las opciones que tenemos en la lista, el string comparison es para validar que no importe si el usuario escribe mayuscula o minuscula 
+
+} while (answer!.Equals("s",StringComparison.CurrentCultureIgnoreCase) );
+Console.WriteLine("F mi loco");
---
+++ Share/ConsoleExtension.cs
@@ -0,0 +1,31 @@
+﻿namespace Share;
+
+public static class ConsoleExtension //se utiliza apra instanciar sin tener que llamar
+{
+    //creamos un metodo el cual podremos utilizar en cual quier momento con las respectivas validaciones
+    public static int GetInt(string message)
+    {
+        Console.Write(message);
+        var numberString= Console.ReadLine();
+        if(int.TryParse(numberString, out int numberInt))
+        {
+            return numberInt;
+        }
+        return 0;  
+    }
+
+    public static string? GetValidOptions(string message, List<string>options)//public static string? la ? es para validar nulidad
+    {
+        Console.Write(message);
+        var answer = Console.ReadLine();
+        if(options.Any(x => x.Equals(answer,StringComparison.CurrentCultureIgnoreCase)))// funcion any para validar si la respuesta del usuario es igual a alguna de las opciones que tenemos en la lista, el string comparison es para validar que no importe si el usuario escribe mayuscula o minuscula
+        {
+            return answer;
+
+        }
+        
+            return null;
+
+         }
+
+}
