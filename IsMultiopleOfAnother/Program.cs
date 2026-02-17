a03f0153feee2d529e3b8b699b7b1efc8bc34d42
Retoto <brecas1820@gmail.com>
2026-02-16 23:04:50 -0500

    is multiple of another

---
+++ IsMultiopleOfAnother/Program.cs
@@ -0,0 +1,15 @@
+﻿using Share;
+do { 
+var a = ConsoleExtension.GetInt("Ingrese el primer numero: ");
+var b = ConsoleExtension.GetInt("Ingrese el segundo numero: ");
+    if (b % a == 0)
+    {
+        Console.WriteLine($"{a}e s multiplo de {b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a}no  es multiplo de {b}");
+
+    }
+
+    } while (true) ;
---
+++ SortNumber2/Program.cs
@@ -0,0 +1,39 @@
+﻿using Share;// con este utilizamos la libreria que creamos en Share
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese el primer numero: ");
+    var b = ConsoleExtension.GetInt("Ingrese el segundo numero: ");
+    var c = ConsoleExtension.GetInt("Ingrese el tercer numero: ");
+
+    if (a > b)
+    {
+        if (b < c)
+        {
+            Console.WriteLine($"El mayor es{a}, el medio es {b} le menor es {c}");
+        }
+        else {
+
+            Console.WriteLine($"El mayor es{a}, el medio es {c} le menor es {b}");
+
+        }
+
+
+    }
+    else if (b > a && b > c)
+    
+    {
+        if (a > c)
+        {
+            Console.WriteLine($"El mayor es{b}, el medio es {a} le menor es {c}");
+        }
+        else 
+        {
+            Console.WriteLine($"El mayor es{b}, el medio es {c} le menor es {a}");
+
+        }
+        
+
+    }
+
+
+} while (true);
