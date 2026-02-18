328de08462e2e24641f8416c455bec1ad0c6d821
Retoto <brecas1820@gmail.com>
2026-02-17 23:33:16 -0500

    IsLeaprYear

---
+++ IsLeapYear/Program.cs
@@ -0,0 +1,57 @@
+﻿using Share;
+do
+{
+    var currentYear = DateTime.Now.Year;//creamos una variable para guardar el año actual del sistema
+    var message= string.Empty;
+    var year = ConsoleExtension.GetInt("Ingrese ano: ");
+
+    //hacemos uan validacion mejor organiozada por la varible nmessage
+    if (year == currentYear) 
+    {
+        message= "es"
+    }
+    else if (year > currentYear)
+    {
+        message= "sera";
+    }
+    else
+    {
+        message= "fue";
+    }
+
+
+
+
+    if (year % 4 == 0)
+
+    {
+        if (year % 100 == 0)
+        {
+            if ( year % 400 ==0) 
+            
+            {
+                Console.WriteLine($"el  {year} Si{message}biciesto");
+            }
+            else
+            {
+                Console.WriteLine($"el  {year} no{message}biciesto");
+            }
+
+        }
+        else 
+        {
+            Console.WriteLine($"el  {year} Si{message}biciesto");
+        }
+
+
+
+
+    }
+    else
+    {
+        Console.WriteLine($"el  { year} no{message}biciesto");
+    }
+
+  
+
+} while (true);
