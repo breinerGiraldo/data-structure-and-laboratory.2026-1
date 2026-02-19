b2aa2329698ee7d6ab89c035ee97f51260777b30
Retoto <brecas1820@gmail.com>
2026-02-18 20:06:43 -0500

    IsLeapYear

--- IsLeapYear/Program.cs
+++ IsLeapYear/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Share;
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };// definimos una lista de opciones para validar la respuesta del usuario
 do
 {
     var currentYear = DateTime.Now.Year;//creamos una variable para guardar el año actual del sistema
@@ -8,7 +10,7 @@ do
     //hacemos uan validacion mejor organiozada por la varible nmessage
     if (year == currentYear) 
     {
-        message= "es"
+        message = "es";
     }
     else if (year > currentYear)
     {
@@ -52,6 +54,12 @@ do
         Console.WriteLine($"el  { year} no{message}biciesto");
     }
 
-  
 
-} while (true);
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Desea continuar [S]i , [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));// validamos que la respuesta del usuario sea igual a alguna de las opciones que tenemos en la lista, el string comparison es para validar que no importe si el usuario escribe mayuscula o minuscula 
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("F mi loco");
--- SortNumber2/Program.cs
+++ SortNumber2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Share;// con este utilizamos la libreria que creamos en Share
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };// definimos una lista de opciones para validar la respuesta del usuario
 do
 {
     var a = ConsoleExtension.GetInt("Ingrese el primer numero: ");
@@ -36,4 +38,11 @@ do
     }
 
 
-} while (true);
+}  
+do
+{
+    answer = ConsoleExtension.GetValidOptions("Desea continuar [S]i , [N]o?: ", options);
+} while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));// validamos que la respuesta del usuario sea igual a alguna de las opciones que tenemos en la lista, el string comparison es para validar que no importe si el usuario escribe mayuscula o minuscula 
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase)) ;
+Console.WriteLine("F mi loco");
