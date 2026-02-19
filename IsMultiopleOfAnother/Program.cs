b375e42c1fff69b68b92e3580fda1af1dabdd46e
Retoto <brecas1820@gmail.com>
2026-02-18 20:46:39 -0500

    IsMultiopleOfAnother

--- IsMultiopleOfAnother/Program.cs
+++ IsMultiopleOfAnother/Program.cs
@@ -1,15 +1,23 @@
 ﻿using Share;
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };// definimos una lista de opciones para validar la respuesta del usuario
 do { 
 var a = ConsoleExtension.GetInt("Ingrese el primer numero: ");
 var b = ConsoleExtension.GetInt("Ingrese el segundo numero: ");
     if (b % a == 0)
     {
-        Console.WriteLine($"{a}e s multiplo de {b}");
+        Console.WriteLine($"{a} es multiplo de {b}");
     }
     else
     {
-        Console.WriteLine($"{a}no  es multiplo de {b}");
+        Console.WriteLine($"{a} no  es multiplo de {b}");
 
     }
 
-    } while (true) ;
+    do
+{
+    answer = ConsoleExtension.GetValidOptions("Desea continuar [S]i , [N]o?: ", options);
+} while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));// validamos que la respuesta del usuario sea igual a alguna de las opciones que tenemos en la lista, el string comparison es para validar que no importe si el usuario escribe mayuscula o minuscula 
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase)) ;
+Console.WriteLine("F mi loco");
--- SortNumber2/Program.cs
+++ SortNumber2/Program.cs
@@ -1,46 +1,51 @@
 ﻿using Share;// con este utilizamos la libreria que creamos en Share
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };// definimos una lista de opciones para validar la respuesta del usuario
+
 do
 {
     var a = ConsoleExtension.GetInt("Ingrese el primer numero: ");
     var b = ConsoleExtension.GetInt("Ingrese el segundo numero: ");
     var c = ConsoleExtension.GetInt("Ingrese el tercer numero: ");
 
-    if (a > b)
+    if (a > b && a > c)
     {
-        if (b < c)
+        if (b > c)
         {
-            Console.WriteLine($"El mayor es{a}, el medio es {b} le menor es {c}");
-        }
-        else {
-
-            Console.WriteLine($"El mayor es{a}, el medio es {c} le menor es {b}");
+            Console.WriteLine($"El mayor es {a} el de el emdio es {b} el emenor es {c}");
 
         }
-
-
+        else
+        {
+            Console.WriteLine($"El mayor es {a}, el de el medio es {c} el menor es {b}");
+        }
     }
     else if (b > a && b > c)
-    
     {
         if (a > c)
         {
-            Console.WriteLine($"El mayor es{b}, el medio es {a} le menor es {c}");
+            Console.WriteLine($"El mayor es {b} el de el medio es {a} el emenor es {c}");
         }
-        else 
+        else
         {
-            Console.WriteLine($"El mayor es{b}, el medio es {c} le menor es {a}");
-
+            Console.WriteLine($"El mayor es {b}, el de el medio es {c} el menor es {a}");
         }
-        
-
     }
+    else
+    {
+        if (a > b)
+        {
+            Console.WriteLine($"El mayor es {c} el de el medio es {a} el emenor es {b}");
+        }
+        else
+        {
+            Console.WriteLine($"El mayor es {c}, el de el medio es {b} el menor es {a}");
+        }
+    }
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Desea continuar [S]i , [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));// validamos que la respuesta del usuario sea igual a alguna de las opciones que tenemos en la lista, el string comparison es para validar que no importe si el usuario escribe mayuscula o minuscula 
 
-
-}  
-do
-{
-    answer = ConsoleExtension.GetValidOptions("Desea continuar [S]i , [N]o?: ", options);
-} while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));// validamos que la respuesta del usuario sea igual a alguna de las opciones que tenemos en la lista, el string comparison es para validar que no importe si el usuario escribe mayuscula o minuscula 
-
-} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase)) ;
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
 Console.WriteLine("F mi loco");
